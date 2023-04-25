87c1b7253ba8cdf1789c208efd8743e50d669242
neelchavan <neelchavan9@gmail.com>
2023-04-25 11:47:41 +0530

    adding more tests to Store Page

--- cypress/integration/PageObjects/StorePage.js
+++ cypress/integration/PageObjects/StorePage.js
@@ -6,6 +6,30 @@ class StorePage {
   getAddToCartButton(index) {
     return cy.get(`.products.columns-4 li:nth-child(${index}) .button`);
   }
+
+  getSearchTextBox() {
+    return cy.get("[type='search']");
+  }
+
+  getSearchBtnAndClick() {
+    return cy.get("[value='Search']").click();
+  }
+
+  getProductCategory() {
+    return cy.get(".ast-woo-product-category");
+  }
+
+  getCategoryDropdownAndSelectVal(value) {
+    return cy.get("#product_cat").select(value);
+  }
+
+  getSortingOptionsAndSelect(value) {
+    return cy.get(".orderby").select(value).should("have.value", value);
+  }
+
+  getProductAmount() {
+    return cy.get(".price ins span bdi");
+  }
 }
 
 export default StorePage;
--- cypress/integration/Tests/StorePageTests.js
+++ cypress/integration/Tests/StorePageTests.js
@@ -31,4 +31,52 @@ describe("Store page tests", () => {
       cy.go("back");
     });
   });
+
+  it("when user searches for a product it should give him the relevent results to his search query", function () {
+    // go to store page from home page
+    homePage.getStoreLinkAndClick();
+    // type the search query you want to perfom and click the search button
+    storePage.getSearchTextBox().type(this.data.searchQuery);
+    storePage.getSearchBtnAndClick();
+    // verify if the results are relevent to the search query
+    storePage.getProductNames().each(function (el) {
+      if (el.text().includes(this.data.searchQuery)) {
+        assert(true);
+      }
+    });
+  });
+
+  it("Verify if the search by category feature gives the relevent results", function () {
+    // go to store page from home page
+    homePage.getStoreLinkAndClick();
+
+    // select category to search
+    storePage.getCategoryDropdownAndSelectVal(this.data.categoryVal);
+
+    // verify if the selected category is giving the relevent results
+    storePage.getProductCategory().each((el) => {
+      expect(el.text().trim()).to.equal("Men");
+    });
+  });
+
+  it("verify when applied the price low to high filter it should sort the products according to it", function () {
+    // go to store page from home page
+    homePage.getStoreLinkAndClick();
+
+    // select the sorting option and verify the result
+    storePage.getSortingOptionsAndSelect(this.data.sortingOption);
+
+    // verify the sorting results
+    let num = 0;
+    storePage.getProductAmount().each((el) => {
+      let amount = Number(el.text().split("$")[1]);
+      if (num < amount) {
+        cy.log(num);
+        num = amount;
+        assert(true);
+      } else {
+        assert(false);
+      }
+    });
+  });
 });
