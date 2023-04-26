8625e1a0f4cdd057535c18981af546063543f33b
neelchavan <neelchavan9@gmail.com>
2023-04-26 10:18:31 +0530

    added html reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter", // for html reports
   env: {
     baseUrl: "https://askomdch.com/",
   },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      screenshotOnRunFailure = true;
+      require("cypress-mochawesome-reporter/plugin")(on); // for html reports
     },
     specPattern: "cypress/integration/Tests/*.js",
   },
